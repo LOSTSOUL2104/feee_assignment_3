c1be8bcf9d16b3bf2c24ad66e98b187acc883d1d
LOSTSOUL2104 <godofwar210405@gmail.com>
2024-04-19 14:09:01 +0530

    New branch

---
+++ src/components/NotesList.jsx
@@ -0,0 +1,20 @@
+import Note from "./Note.jsx";
+import AddNote from "./AddNote.jsx";
+
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => (
+        <Note
+          id={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
+      ))}
+      <AddNote handleAddNote={handleAddNote} />
+    </div>
+  );
+};
+
+export default NotesList;
